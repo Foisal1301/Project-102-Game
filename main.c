fc06d2bb4f07e1475c42188ef1583e2177d1af39
Mohammad Foisal <abbasnurul805@gmail.com>
2026-08-01 21:15:56 +0600

    Update main.c

--- main.c
+++ main.c
@@ -9,9 +9,7 @@ int main(void)
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Swaaaaaaaar!", 190, 200, 20, LIGHTGRAY);
-        DrawText("Yo check check", 190, 200, 20, LIGHTGRAY);
-        DrawText("East or West,I am the best!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Faah!", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
 
