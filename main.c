545f1a458453fdd12a205a73f0df1431f8103506
Mohammad Foisal <81463825+Foisal1301@users.noreply.github.com>
2026-08-01 21:20:46 +0600

    Update main.c

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(void)
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Faah!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Yo!", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
 
