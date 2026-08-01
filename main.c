5b2f17da04a6889cb33f14f46694de7657b45cde
Mohammad Foisal <abbasnurul805@gmail.com>
2026-08-01 22:05:41 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,23 @@
 #include "raylib.h"
 
+#define HEIGHT 600
+#define WIDTH 800
 int main(void)
 {
-    InitWindow(800, 600, "Game");
+    InitWindow(WIDTH, HEIGHT, "Game");
     SetTargetFPS(60);
+
+    float ground = 3*HEIGHT/4;
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Never ever!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Never ever!", 190, 200, 80, LIGHTGRAY);
+
+        Rectangle groundRect = {0,ground,WIDTH,HEIGHT-ground};
+        DrawRectangleRec(groundRect,GetColor(0x014B43FF));
+        DrawRectangleLinesEx(groundRect,3,BLACK);
         EndDrawing();
     }
 
