3d0b59facbf356406bb04e8a02da69a844a84402
Mohammad Foisal <abbasnurul805@gmail.com>
2026-08-01 21:05:47 +0600

    Update main.c

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(void)
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Faah!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Swaaaaaaaar!", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
 
