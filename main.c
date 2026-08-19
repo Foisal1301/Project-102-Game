6e18c009cb3611db43897124bd2321c6fe0e7c88
SajidSwapno <shahariarsajid12@gmail.com>
2026-08-19 17:20:14 +0600

    balls
    sprites

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include "raylib.h"
+#include <stdio.h>
 
 #define HEIGHT 600
 #define WIDTH 800
+#define BALLROWS 6
+#define BALLCOLS 14
 
+// PAGES {"Menu","RESUME","ABOUT","GAMEPLAY"}
+
+int pageIndex = 3;
 int main(void)
 {
     InitWindow(WIDTH, HEIGHT, "Game");
@@ -15,6 +21,34 @@ int main(void)
         // ClearBackground(RAYWHITE);
 
         DrawTexture(bg,0,0,WHITE);
+
+        switch (pageIndex)
+        {
+        case 3:
+
+        //loding ball images
+
+        for (int i = 0; i < 2; i++)
+        {
+            char path[50];
+            sprintf(path, "assets/ball_%d_small_alt.png", i+1);
+            
+        }
+        
+
+            int balls[BALLROWS][BALLCOLS];
+            for (int i = 0; i < BALLROWS; i++)
+            {
+                for (int j = 0; j < BALLCOLS; j++)
+                {
+                    
+                }
+                
+            }
+            
+
+            break;
+        }
         
         EndDrawing();
     }
