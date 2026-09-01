ce01f8a47b0c6a6d3587212f8be859fcd79cf9f7
Mohammad Foisal <abbasnurul805@gmail.com>
2026-09-01 22:49:53 +0600

    updated

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "raylib.h"
 #include "raymath.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 
 #define DEBUG 1
 #define HEIGHT 600
@@ -63,8 +63,8 @@ int main(void)
     }
 
     // Shooter
-    // bool shooted = false;
-    Vector2 cannonPos = {WIDTH / 2 - 40,HEIGHT - 130};
+    bool shooted = false;
+    Vector2 cannonBase = {WIDTH / 2.0f,HEIGHT * 0.95f};
     Vector2 cannonOrigin = {40,120};
     float cannonAngle = 0;
     Texture2D shooters[3];
@@ -75,11 +75,14 @@ int main(void)
     }
     int shooterIndex = GetRandomValue(0,1);
 
-    // char path[50];
-    // sprintf(path,"assets/shooter_%d.png",shooterIndex+1);
-    // Texture2D bullet = LoadTexture(path);
-    // Vector2 bulletPosition = {0,0};
-    // Vector2 bulletVelocity = {0,0};
+    Texture2D bullets[2];
+    for(int i=1;i<=2;i++){
+        char path[50];
+        sprintf(path,"assets/ball_%d.png",i);
+        bullets[i-1] = LoadTexture(path);
+    }
+    Vector2 bulletPosition = {0,0};
+    Vector2 bulletVelocity = {0,0};
 
     while (!WindowShouldClose() && !exit)
     {
@@ -196,60 +199,65 @@ int main(void)
 
             // shooter
             Vector2 mousePos = GetMousePosition();
-            float dx = mousePos.x - cannonPos.x;
-            float dy = mousePos.y - cannonPos.y;
-            float tempAngle = (atan2f(dy,dx) * RAD2DEG)+90;
-            if(tempAngle<=70 && tempAngle>=-65){
+            float dx = mousePos.x - cannonBase.x;
+            float dy = mousePos.y - cannonBase.y;
+            float tempAngle = (atan2f(dy,dx) * RAD2DEG);
+            // if(tempAngle<=70 && tempAngle>=-65)
                 cannonAngle = tempAngle;
-            }
             DrawTexturePro(
                 shooters[shooterIndex],
                 (Rectangle){0,0,shooters[shooterIndex].width,shooters[shooterIndex].height},
                 (Rectangle){
-                    cannonPos.x+50,//WIDTH / 2 - 40,
-                    cannonPos.y+100,//HEIGHT - 130,
+                    cannonBase.x,// cannonPos.x+50,//WIDTH / 2 - 40,
+                    cannonBase.y,// cannonPos.y+100,//HEIGHT - 130,
                     80,
                     120
                 },
-                cannonOrigin ,cannonAngle,WHITE
+                cannonOrigin ,cannonAngle+90,WHITE
             );
-            shooterIndex = GetRandomValue(0,1);
-            // if (IsKeyPressed(KEY_SPACE)) {
-            //     if(!shooted){
-            //         shooted=true;
-            //         bulletPosition.x = cannonPos.x;
-            //         bulletPosition.y = cannonPos.y;
-            //         bulletVelocity.x = -5;
-            //         bulletVelocity.y = -5;
-            //         // printf("%f %f\n",GetMousePosition().x,GetMousePosition().y);
-            //     }
-            // }
+            if (IsKeyPressed(KEY_SPACE)) {
+                if(!shooted){
+                    shooted=true;
+                    bulletPosition.x = cannonBase.x;
+                    bulletPosition.y = cannonBase.y;
+                    printf("%f\n",cannonAngle);
+                    bulletVelocity.x = 20*cos(cannonAngle);
+                    bulletVelocity.y = 20*sin(cannonAngle);
+                    printf("%f\n",sin(3.1416/2));
+                    if(bulletVelocity.y>0) bulletVelocity.y = -bulletVelocity.y;
+                   printf("%f %f\n",GetMousePosition().x,GetMousePosition().y);
+                }
+            }
 
-            // if(shooted){
-            //     bulletPosition.x += bulletVelocity.x;
-            //     bulletPosition.y += bulletVelocity.y;
-            //     if(bulletPosition.x<0){
-            //         bulletPosition.x = 0;
-            //         bulletVelocity.x = -bulletVelocity.x;
-            //     }
-            //     if(bulletPosition.y<0){
-            //         bulletPosition.y = 0;
-            //         bulletVelocity.x = 0;
-            //         bulletVelocity.y = 0;
-            //         shooted = false;
-            //     }
-            //     if(bulletPosition.x>800){
-            //         bulletPosition.x = 800;
-            //         bulletVelocity.x = -bulletVelocity.x;
-            //     }
+            if(shooted){
+                bulletPosition.x += bulletVelocity.x;
+                bulletPosition.y += bulletVelocity.y;
+                if(bulletPosition.x<0){
+                    bulletPosition.x = 0;
+                    bulletVelocity.x = -bulletVelocity.x;
+                }else if(bulletPosition.x>WIDTH){
+                    bulletPosition.x = WIDTH;
+                    bulletVelocity.x = -bulletVelocity.x;
+                }
+                if(bulletPosition.y<0){
+                    bulletPosition.y = 0;
+                    bulletVelocity.x = 0;
+                    bulletVelocity.y = 0;
+                    shooterIndex = GetRandomValue(0,1);
+                    shooted = false;
+                }
+                if(bulletPosition.x>800){
+                    bulletPosition.x = 800;
+                    bulletVelocity.x = -bulletVelocity.x;
+                }
                 
-            //     DrawTexturePro(
-            //         bullet,
-            //         (Rectangle){0,0,bullet.width,bullet.height},
-            //         (Rectangle){bulletPosition.x,bulletPosition.y,BALLRADIUS * 2, BALLRADIUS * 2},
-            //         Vector2Zero(),0,WHITE
-            //     );
-            // }
+                DrawTexturePro(
+                    bullets[shooterIndex],
+                    (Rectangle){0,0,bullets[shooterIndex].width,bullets[shooterIndex].height},
+                    (Rectangle){bulletPosition.x,bulletPosition.y,BALLRADIUS * 2, BALLRADIUS * 2},
+                    Vector2Zero(),0,WHITE
+                );
+            }
 
             // score
             char scores[30];
